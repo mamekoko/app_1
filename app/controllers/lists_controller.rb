213d1c6f728984ae09621df2ea3936d38b698af9
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-04 12:48:55 +0000

    sample_app_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
@@ -8,6 +15,12 @@ class ListsController < ApplicationController
   def show
   end
 
-  def edit
+
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
