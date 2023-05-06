9f6311827e614422de1babc7590908f43d0f368b
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-06 07:50:32 +0000

    sample_app_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,6 +16,16 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
 
 
 
